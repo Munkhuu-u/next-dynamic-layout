f25f3cf0d2a9b4cda77c5e284d9f5806e4d8442d
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2024-01-26 15:24:59 +0800

    class work

---
+++ src/pages/prod/index.js
@@ -0,0 +1,14 @@
+export default function Index() {
+  return (
+    <div>
+      <div>
+        <p>Product page</p>
+      </div>
+      <div className="flex justify-around">
+        <button>ONE</button>
+        <button>TWO</button>
+        <button>THREE</button>
+      </div>
+    </div>
+  );
+}
